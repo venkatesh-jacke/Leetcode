b303518c74903734dbb8b9d0155bb385c7eccd24
venkatesh-jacke <venkateshjacke@gmail.com>
2024-01-15 10:14:37 +0530

    1929-Concatenation of Array

--- src/Arrays/ConcatenationOfArray.java
+++ src/Arrays/ConcatenationOfArray.java
@@ -1,4 +1,25 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class ConcatenationOfArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 1};
+        System.out.println(Arrays.toString(getConcatenation(arr)));
+
+    }
+
+    static int[] getConcatenation(int[] nums) {
+        int arr[]=new int[2*nums.length];
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            arr[i]=nums[i];
+            arr[n+i]=nums[i];
+        }
+        return arr;
+    }
+
 }
+
+//Time complexity  O(n)  Traverse all elemnets in an array
+//Space complexity O(n)  2 * size of original array
