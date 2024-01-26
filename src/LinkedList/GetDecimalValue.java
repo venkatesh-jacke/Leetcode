4feeb6d11d66977f481e2759292b6789d74e3ac7
venkatesh-jacke <venkateshjacke@gmail.com>
2024-01-26 20:21:50 +0530

    1290. Convert Binary Number in a Linked List to Integer

---
+++ src/LinkedList/GetDecimalValue.java
@@ -0,0 +1,45 @@
+package LinkedList;
+
+
+//1290. Convert Binary Number in a Linked List to Integer
+public class GetDecimalValue {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(0);
+        head.next.next = new ListNode(1);
+
+        // Call the getDecimalValue method and print the result
+        int result = getDecimalValue(head);
+        System.out.println("Decimal value: " + result);
+    }
+
+
+    //Time Complexity is O(n)
+    //Space Complexity is O(1)
+    static public int getDecimalValue(ListNode head) {
+        int res = 0;
+        while (head != null) {
+            res = res * 2 + head.val;
+            head = head.next;
+        }
+        return res;
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
