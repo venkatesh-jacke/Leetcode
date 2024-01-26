48993184b3a979bfbeba6fe817af8d5a526f6103
venkatesh-jacke <venkateshjacke@gmail.com>
2024-01-26 20:57:13 +0530

    ListNode kept in public to access all files with unique name

---
+++ src/LinkedList/ListNode.java
@@ -0,0 +1,27 @@
+package LinkedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+
+  public static void printList(ListNode head) {
+        while(head!=null){
+            System.out.print(head.val+"->");
+            head=head.next;
+        }
+        System.out.print("Null");
+    }
+}
