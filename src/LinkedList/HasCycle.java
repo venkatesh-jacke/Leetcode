d31b0c67fe97dcdf9be4a69fe59b7d2aac9ab460
venkatesh-jacke <venkateshjacke@gmail.com>
2024-01-28 17:28:42 +0530

    141. Linked List Cycle

---
+++ src/LinkedList/HasCycle.java
@@ -0,0 +1,26 @@
+package LinkedList;
+
+
+//141. Linked List Cycle
+public class HasCycle {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(1);
+        head.next.next.next = head;
+        System.out.println(hasCycle(head));
+    }
+
+    static public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow) return true;
+        }
+        return false;
+
+    }
+}
