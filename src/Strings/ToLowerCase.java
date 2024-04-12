c8181957cc62b206272bc955157bdac492d89f2f
venkatesh-jacke <venkateshjacke@gmail.com>
2024-04-12 10:48:29 +0530

    709. To Lower Case

---
+++ src/Strings/ToLowerCase.java
@@ -0,0 +1,12 @@
+package Strings;
+
+
+//709. To Lower Case
+public class ToLowerCase {
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("LOVELY"));
+    }
+   static public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
