2ea23f7bc91e5d1f9779ef51ffad3a76e4e97700
venkatesh-jacke <venkateshjacke@gmail.com>
2024-04-12 09:59:51 +0530

    1816. Truncate Sentence

---
+++ src/Strings/TruncateSentence.java
@@ -0,0 +1,23 @@
+package Strings;
+
+
+//1816. Truncate Sentence
+public class TruncateSentence {
+    public static void main(String[] args) {
+        String s = "Hello how are you Contestant";
+        int k = 4;
+        System.out.println(truncateSentence(s, k));
+    }
+
+    static public String truncateSentence(String s, int k) {
+        int index = 0;
+        int count = 0;
+        for (index = 0; index < s.length(); index++) {
+            if (s.charAt(index) == ' ')
+                count++;
+            if (count == k)
+                break;
+        }
+        return s.substring(0, index);
+    }
+}
