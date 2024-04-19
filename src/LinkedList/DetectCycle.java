6b2e6d98ab2bd6218ff5e0e3e05aa5ef9c0be045
venkatesh-jacke <venkateshjacke@gmail.com>
2024-04-19 20:30:10 +0530

    142. Linked List Cycle II

---
+++ src/LinkedList/DetectCycle.java
@@ -0,0 +1,34 @@
+package LinkedList;
+
+
+//142. Linked List Cycle II
+public class DetectCycle {
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = head.next;
+        assert detectCycle(head) != null;
+        System.out.println(detectCycle(head).val);
+    }
+
+    static public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                fast = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return fast;
+            }
+        }
+        return null;
+    }
+}
