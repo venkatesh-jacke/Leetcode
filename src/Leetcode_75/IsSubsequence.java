791f7fa4782e4a45847bc0eef687b1d6b1cde880
venkatesh-jacke <venkateshjacke@gmail.com>
2024-06-13 20:26:01 +0530

    392. Is Subsequence

---
+++ src/Leetcode_75/IsSubsequence.java
@@ -0,0 +1,23 @@
+package Leetcode_75;
+
+
+//392. Is Subsequence
+public class IsSubsequence {
+    public static void main(String[] args) {
+        String s = "abc";
+        String t = "ahbgdc";
+        System.out.println(isSubsequence(s, t));
+    }
+
+    static public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+}
