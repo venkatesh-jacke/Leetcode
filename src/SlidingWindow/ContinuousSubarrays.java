f2f66b3745f3e4d94079c7fab18761f4dc360843
venkatesh-jacke <venkateshjacke@gmail.com>
2025-08-23 12:45:20 +0530

    2762. Continuous Subarrays

--- src/SlidingWindow/ContinuousSubarrays.java
+++ src/SlidingWindow/ContinuousSubarrays.java
@@ -2,28 +2,28 @@ package SlidingWindow;
 
 import java.util.TreeMap;
 
+//2762. Continuous Subarrays
+
 public class ContinuousSubarrays {
     public static void main(String[] args) {
         int[] nums = {5, 4, 2, 4};
         System.out.println(continuousSubarrays(nums));
     }
 
-    static  public long continuousSubarrays(int[] nums) {
+    static public long continuousSubarrays(int[] nums) {
         long count = 0;
         int left = 0, right = 0, n = nums.length;
-        TreeMap<Integer, Integer> freq = new TreeMap<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         while (right < n) {
-            freq.put(nums[right], freq.getOrDefault(nums[right], 0) + 1);
-            while (freq.lastEntry().getKey() - freq.firstEntry().getKey() > 2) {
-                freq.put(nums[left], freq.get(nums[left]) - 1);
-                if (freq.get(nums[left]) == 0) {
-                    freq.remove(nums[left]);
-                }
-                left++;
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+            while (map.lastKey() - map.firstKey() > 2) {
+                map.put(nums[left], map.getOrDefault(nums[left], 0) - 1);
+                if (map.get(nums[left]) == 0) map.remove(nums[left]);
             }
-            count += right - left + 1;
+            count += (right - left + 1);
             right++;
         }
+
         return count;
     }
 }
