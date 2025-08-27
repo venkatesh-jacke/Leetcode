651cff9b501f8e84d30ee43de8523612ef66ccf3
venkatesh-jacke <venkateshjacke@gmail.com>
2025-08-27 12:45:37 +0530

    962. Maximum Width Ramp

--- src/SlidingWindow/MaxWidthRamp.java
+++ src/SlidingWindow/MaxWidthRamp.java
@@ -1,28 +1,31 @@
-package SlidingWindow;
+    package SlidingWindow;
 
 
-import java.util.Arrays;
+    import java.util.Arrays;
 
-//962. Maximum Width Ramp
-public class MaxWidthRamp {
+    //962. Maximum Width Ramp
+    public class MaxWidthRamp {
 
-    public static void main(String[] args) {
-        int[] nums = {9,8,1,0,1,9,4,0,4,1};
-        System.out.println(maxWidthRamp(nums));
-    }
-    static public int maxWidthRamp(int[] nums) {
-        int n = nums.length,res=0;
-        int[] maxRight= new int[n];
-        maxRight[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            maxRight[i]=Math.max(nums[i],maxRight[i+1]);
+        public static void main(String[] args) {
+            int[] nums = {9, 8, 1, 0, 1, 9, 4, 0, 4, 1};
+            System.out.println(maxWidthRamp(nums));
         }
-        System.out.println(Arrays.toString(maxRight));
-        for (int l = 0, r = 0; r < n; r++) {
-            while (nums[l] > maxRight[r])
-                l+=1;
-            res = Math.max(res, r - l);
+
+        static public int maxWidthRamp(int[] nums) {
+            int n = nums.length, res = 0;
+            int[] maxRight = new int[n];
+            maxRight[n - 1] = nums[n - 1];
+            //Precompute the max right
+            for (int i = n - 2; i >= 0; i--) {
+                maxRight[i] = Math.max(nums[i], maxRight[i + 1]);
+            }
+            System.out.println(Arrays.toString(maxRight));
+            for (int l = 0, r = 0; r < n; r++) {
+                //if left side value is greater than invalid
+                while (nums[l] > maxRight[r])
+                    l += 1;
+                res = Math.max(res, r - l);
+            }
+            return res;
         }
-        return res;
     }
-}
