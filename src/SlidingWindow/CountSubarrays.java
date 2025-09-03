4d1791d2d04b774ccf79bb86f77d59b3f6891352
venkatesh-jacke <venkateshjacke@gmail.com>
2025-09-03 17:22:33 +0530

    2962. Count Subarrays Where Max Element Appears at Least K Times

---
+++ src/SlidingWindow/CountSubarrays.java
@@ -0,0 +1,29 @@
+package SlidingWindow;
+
+//2962. Count Subarrays Where Max Element Appears at Least K Times
+public class CountSubarrays {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 3, 3};
+        int k = 2;
+        System.out.println(countSubarrays(arr, k));
+    }
+
+    static public long countSubarrays(int[] nums, int k) {
+        long ans = 0;
+        int n = nums.length;
+        int maxElement = 0;
+        for (int i : nums) maxElement = Math.max(maxElement, i);
+        int maxFreq = 0;
+        for (int r = 0, l = 0; r < n; r++) {
+            if (nums[r] == maxElement) maxFreq++; //monitoring only the maxElement in array
+            //if condition satisfies than calculate the ans
+            while (l < r && maxFreq > k) {
+                ans += (n - l); //// all subarrays starting at index l (till the end of array) are invalid
+                if (nums[l] == maxElement) maxFreq--;
+                l++;
+            }
+        }
+
+        return ans;
+    }
+}
