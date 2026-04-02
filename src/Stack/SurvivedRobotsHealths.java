d33d6cccb8d9375fbe0e56fba1621415a02ee455
Venkatesh Ezhumalai <venkateshjacke@gmail.com>
2026-04-02 19:11:47 +0530

    2751. Robot Collisions

--- src/Stack/SurvivedRobotsHealths.java
+++ src/Stack/SurvivedRobotsHealths.java
@@ -6,31 +6,6 @@ import java.util.*;
 //2751. Robot Collisions
 public class SurvivedRobotsHealths {
 
-    static class Robot implements Comparable<Robot> {
-        int id;
-
-        int pos;
-        int health;
-        char dir;
-
-        public Robot(int id, int pos, int health, char dir) {
-            this.id = id;
-            this.pos = pos;
-            this.health = health;
-            this.dir = dir;
-        }
-
-        @Override
-        public String toString() {
-            return "id:" + this.id + " " + "pos:" + this.pos + " " + "health:" + this.health + " " + "dir:" + this.dir;
-        }
-
-        @Override
-        public int compareTo(Robot other) {
-            return Integer.compare(this.pos, other.pos);
-        }
-    }
-
     public static void main(String[] args) {
         int[] pos = {3, 5, 2, 6};
         int[] health = {10, 10, 15, 12};
@@ -41,33 +16,40 @@ public class SurvivedRobotsHealths {
     static public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String dir) {
         int n = positions.length;
         List<Integer> res = new ArrayList<>();
+        // Stack to store indices of robots moving to the right
         Stack<Integer> stack = new Stack<>();
+
         Integer[] indices = new Integer[n];
         for (int i = 0; i < n; i++) indices[i] = i;
+        // Sort indices based on positions (important for collision order)
         Arrays.sort(indices, (l, r) -> Integer.compare(positions[l], positions[r]));
 
         for (int cur : indices) {
             if (dir.charAt(cur) == 'R') {
                 stack.push(cur);
             } else {
+                // Robot moving left → possible collisions with previous right-moving robots
                 while (!stack.isEmpty() && healths[cur] > 0) {
-                    int top = stack.pop();
+                    int top = stack.pop(); // Last right-moving robot
+                    // Case 1: Right robot stronger
                     if (healths[top] > healths[cur]) {
                         healths[top] -= 1;
                         healths[cur] = 0;
                         stack.push(top);
-
-                    } else if (healths[top] < healths[cur]) {
+                    }
+                    // Case 2: Left robot stronger
+                    else if (healths[top] < healths[cur]) {
                         healths[cur] -= 1;
                         healths[top] = 0;
-                    } else {
+                    }
+                    // Case 3: Equal health → both destroyed
+                    else {
                         healths[cur] = 0;
                         healths[top] = 0;
                     }
                 }
             }
         }
-        System.out.println(Arrays.toString(healths));
         for (int i = 0; i < n; i++) {
             if (healths[i] > 0) {
                 res.add(healths[i]);
